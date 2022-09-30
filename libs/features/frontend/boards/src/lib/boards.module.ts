39f05336c336e721941d0e68b83ef1d96781c7c4
yasserkharab <kharabyasser@icloud.com>
2022-09-30 00:59:18 -0400

    filtering and sticky header

--- libs/features/frontend/boards/src/lib/boards.module.ts
+++ libs/features/frontend/boards/src/lib/boards.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { BoardsComponent } from './components/boards/boards.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 
 
 @NgModule({
-  imports: [CommonModule, MatTableModule, MatPaginatorModule],
+  imports: [CommonModule, MatTableModule, MatPaginatorModule, MatSortModule],
   declarations: [BoardsComponent],
   exports: [BoardsComponent]
 })
-export class BoardsModule {}
+export class BoardsModule { }
