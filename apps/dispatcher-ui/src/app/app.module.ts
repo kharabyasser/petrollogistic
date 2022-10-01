0072549016ade1cbfa4aa4febb9bdeaadd2ed2cc
yasserkharab <kharabyasser@icloud.com>
2022-10-01 14:41:56 -0400

    app module

--- apps/dispatcher-ui/src/app/app.module.ts
+++ apps/dispatcher-ui/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BoardsModule } from '../../../../libs/features/frontend/boards/src/lib/boards.module';
+import { BoardsModule } from '@petrologistic/features/frontend/boards';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
