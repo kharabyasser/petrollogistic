44331994827f14566ceaff8229934b69ab2f2a86
Yasser Kharab <42950969+kharabyasser@users.noreply.github.com>
2022-10-16 01:11:30 -0400

    detail component

--- libs/features/frontend/boards/src/lib/boards.module.ts
+++ libs/features/frontend/boards/src/lib/boards.module.ts
@@ -6,10 +6,18 @@ import { InputTextModule } from 'primeng/inputtext';
 import { BrowserModule } from '@angular/platform-browser';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { FormsModule } from '@angular/forms';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
-  imports: [TableModule, ButtonModule, InputTextModule, BrowserModule, MultiSelectModule, FormsModule],
-  declarations: [BoardsComponent],
-  exports: [BoardsComponent]
+  imports: [
+    TableModule,
+    ButtonModule,
+    InputTextModule,
+    BrowserModule,
+    MultiSelectModule,
+    FormsModule,
+  ],
+  declarations: [BoardsComponent, DetailComponent],
+  exports: [BoardsComponent],
 })
-export class BoardsModule { }
+export class BoardsModule {}
