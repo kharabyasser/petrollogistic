7a08d48dfd243233bb6de9cd815ca0223069ee30
yasserkharab <kharabyasser@icloud.com>
2022-10-18 22:12:37 -0400

    appmodule

--- apps/dispatcher-ui/src/app/app.module.ts
+++ apps/dispatcher-ui/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BoardsModule } from '@petrologistic/';
+import { BoardsModule } from '@petrologistic/features/frontend/boards';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
