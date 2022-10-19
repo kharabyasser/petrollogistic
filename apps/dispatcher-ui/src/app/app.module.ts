1d1c37eb186bffa573e413fbda1e121a0e6606a6
yasserkharab <kharabyasser@icloud.com>
2022-10-18 22:09:58 -0400

    moving projects

--- apps/dispatcher-ui/src/app/app.module.ts
+++ apps/dispatcher-ui/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BoardsModule } from '@petrologistic/features/frontend/boards';
+import { BoardsModule } from '@petrologistic/';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
