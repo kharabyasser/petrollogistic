729d191e68324d7480f5f9ed715259639ca28bdc
yasserkharab <kharabyasser@icloud.com>
2022-10-27 01:32:23 -0400

    tag filtering

--- libs/features/boards/frontend/src/lib/components/boards/boards.component.ts
+++ libs/features/boards/frontend/src/lib/components/boards/boards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FilterService, SelectItem } from 'primeng/api';
 import { Table } from 'primeng/table';
 import { DeliveryRequest } from '../../domain/deliveryrequest';
 import { DeliveryRequestService } from '../../services/deliveryrequest-service';
@@ -13,15 +14,23 @@ export class BoardsComponent implements OnInit {
   selectedRequests: DeliveryRequest[] = [];
 
   cols: any[] = [];
-
+  tags:SelectItem [] = [];
   _selectedColumns: any[] = [];
 
-  constructor(private DeliveryRequestsService: DeliveryRequestService) {}
+  matchModeOptions: SelectItem[] = [];
+
+  constructor(private DeliveryRequestsService: DeliveryRequestService, private filterService: FilterService) {}
 
   ngOnInit() {
     this.DeliveryRequestsService.getDeliveryRequests().subscribe((result) => {
       this.deliveryRequests = [...result.data.deliveryRequests];
-      console.log(result.data.deliveryRequests);
+      this.tags = this.deliveryRequests
+      .flatMap(d => d.tags)
+      .filter((value, index, array) => 
+      {
+        return array.indexOf(value) === index;        
+      })
+      .map(tag => ({ label: tag, value: tag } as SelectItem));
     });
 
     this.cols = [
@@ -37,6 +46,48 @@ export class BoardsComponent implements OnInit {
     ];
 
     this._selectedColumns = this.cols;
+
+    const containsTagFilterName = 'contains-tag';
+    const exactMatchTagFilterName = 'exact-match-tag';
+
+        this.filterService.register(containsTagFilterName, (value: string[], filter: string[]): boolean => {
+            if (filter === undefined || filter === null || filter.length === 0) {
+                return true;
+            }
+
+            if (value === undefined || value === null) {
+                return false;
+            }
+
+            return filter.every(elem => value.includes(elem));
+        });
+
+        this.filterService.register(exactMatchTagFilterName, (value: string[], filter: string[]): boolean => {
+          if (filter === undefined || filter === null || filter.length === 0) {
+              return true;
+          }
+
+          if (value === undefined || value === null) {
+              return false;
+          }
+
+          if (value.length === filter.length) {
+            return value.every(element => {
+              if (filter.includes(element)) {
+                return true;
+              }
+        
+              return false;
+            });
+          }
+        
+          return false;
+      });
+
+       this.matchModeOptions = [
+          { label: 'Contains', value: containsTagFilterName },
+          { label: 'Exact Match', value: exactMatchTagFilterName }
+      ];
   }
 
   get selectedColumns(): any {
