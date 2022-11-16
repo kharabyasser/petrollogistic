02acc341d017c1abffcb9958cc42b0d0aa31e9a2
yasserkharab <kharabyasser@icloud.com>
2022-11-16 00:33:03 -0500

    entities

--- libs/features/boards/frontend/src/lib/+state/delivery-requests-reducer.ts
+++ libs/features/boards/frontend/src/lib/+state/delivery-requests-reducer.ts
@@ -1,30 +1,33 @@
+import { createEntityAdapter, EntityAdapter, EntityState } from "@ngrx/entity";
 import { createReducer, on } from "@ngrx/store";
 import { DeliveryRequest } from "../domain/deliveryrequest";
 
 import * as RequestsActions from './delivery-requests-actions';
 
-export interface State {
-    deliveries: DeliveryRequest[];
+export interface State extends EntityState<DeliveryRequest> {
     isLoading: boolean;
     isLoaded: boolean;
     error: string | null;
 }
 
-export const initialState: State = {
-    deliveries: [],
+export const adapter : EntityAdapter<DeliveryRequest> = 
+   createEntityAdapter<DeliveryRequest>({
+
+   });
+
+export const initialState: State = adapter.getInitialState({
     isLoading: false,
     isLoaded: false,
     error: null
-};
+});
 
 export const reducers = createReducer(
     initialState, 
     on(RequestsActions.getDeliveryRequests, (state) => ({ ...state, isLoading: true })),
-    on(RequestsActions.getDeliveryRequestsSuccess, (state, action) => ({
+    on(RequestsActions.getDeliveryRequestsSuccess, (state, action) => adapter.addMany(action.data, {
          ...state, 
          isLoading: false, 
          isLoaded: true,
-         deliveries: action.data
         })),
     on(RequestsActions.getDeliveryRequestsFailure, (state, action) => ({
          ...state, 
--- libs/features/boards/frontend/src/lib/+state/delivery-requests.selectors.ts
+++ libs/features/boards/frontend/src/lib/+state/delivery-requests.selectors.ts
@@ -1,9 +1,11 @@
 import { createFeatureSelector, createSelector } from "@ngrx/store";
-import { State } from "./delivery-requests-reducer";
+import { adapter, State } from "./delivery-requests-reducer";
 
 export const selectState = createFeatureSelector<State>('DeliveryRequests');
 
-export const requestsSelector = createSelector(selectState, (state: State) => state.deliveries); 
+export const selector = adapter.getSelectors();
+
+export const requestsSelector = createSelector(selectState, selector.selectAll); 
 export const isLoadingSelector = createSelector(selectState, (state: State) => state.isLoading); 
 export const isLoadedSelector = createSelector(selectState, (state: State) => state.isLoaded); 
 export const errorSelector = createSelector(selectState, (state: State) => state.error); 
