fd893a18e57d4aa4b8aec07924a5b160f76bcdac
yasserkharab <kharabyasser@icloud.com>
2022-11-28 23:27:54 -0500

    maps with maplibre

--- libs/features/boards/frontend/src/lib/components/maps/maps.component.ts
+++ libs/features/boards/frontend/src/lib/components/maps/maps.component.ts
@@ -1,28 +1,34 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
-import { Map } from 'maplibre-gl';
+import { Component, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { Map, Marker } from 'maplibre-gl';
+import { DeliveryRequestsFacade } from '../../+state/delivery-requests-facade';
 
 @Component({
   selector: 'petrologistic-maps',
   templateUrl: './maps.component.html',
   styleUrls: ['./maps.component.scss']
 })
-export class MapsComponent implements OnInit, AfterViewInit, OnDestroy {
+export class MapsComponent implements AfterViewInit, OnDestroy {
 
   map: Map | undefined;
   @ViewChild('map') private mapContainer!: ElementRef<HTMLElement>;
 
-  ngOnInit(): void {
+  constructor(private deliveriesFacade: DeliveryRequestsFacade) {
   }
 
   ngAfterViewInit() {
-    const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
+    const initialState = { lng: 139.7525, lat: 35.6846, zoom: 14 };
 
     this.map = new Map({
       container: this.mapContainer.nativeElement,
       style: `https://api.maptiler.com/maps/streets-v2/style.json?key=9GLc7lJKzQasVymrF28T`,
       center: [initialState.lng, initialState.lat],
-      zoom: initialState.zoom
+      zoom: initialState.zoom,
+      attributionControl: false
     });
+
+    new Marker({color: "#FF0000"})
+    .setLngLat([initialState.lng, initialState.lat])
+    .addTo(this.map);
   }
 
   ngOnDestroy() {
